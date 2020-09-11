88cc888be375c226512cf6672514bdf780c801f6
subhamManU <subhamsaha90@gmail.com>
2020-09-11 18:40:00 +0530

    App name replacement across project

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Locusnine-Sales-Dashboard-App app is running!');
+    expect(page.getTitleText()).toEqual('Todo-Management-Dashboard-App app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Locusnine-Sales-Dashboard-App'`, () => {
+  it(`should have as title 'Todo-Management-Dashboard-App'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Locusnine-Sales-Dashboard-App');
+    expect(app.title).toEqual('Todo-Management-Dashboard-App');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Locusnine-Sales-Dashboard-App app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Todo-Management-Dashboard-App app is running!');
   });
 });
